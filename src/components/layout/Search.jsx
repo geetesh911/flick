b5c60fe087b0c9ddba4c2be4e58343757f0befbf
Geetesh Laddha <geetesh911@gmail.com>
2020-06-16 18:57:50 +0530

    provider filtering added

--- src/components/layout/Search.jsx
+++ src/components/layout/Search.jsx
@@ -6,7 +6,7 @@ import $ from "jquery";
 
 export const Search = () => {
   const [query, setQuery] = useState("");
-  const [load, setLoad] = useState(true);
+  const [load, setLoad] = useState(false);
   const [activeProvider, setActiveProvider] = useState("");
 
   const searchContext = useContext(SearchContext);
